09a7bd2ffacbeb78fe4b96cdfb11e0611a612bad
Hari kumar <hari70319@gmail.com>
2024-07-22 18:23:20 +0530

    Fixed timestamp

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -32,11 +32,11 @@ export const stockNames: any = {
 export const formatDate = (timestamp: string): string => {
   const date = new Date(timestamp);
 
+  const d = date.getDate().toFixed().padStart(2, "0");
   const month = (date.getMonth() + 1).toString().padStart(2, "0");
-  const year = date.getFullYear().toString().slice(-2);
   const hours = date.getHours().toString().padStart(2, "0");
   const minutes = date.getMinutes().toString().padStart(2, "0");
   const seconds = date.getSeconds().toString().padStart(2, "0");
 
-  return `${month}/${year}, ${hours}:${minutes}:${seconds}`;
+  return `${d}/${month}, ${hours}:${minutes}:${seconds}`;
 };
